74779078c6e4e7f1ba1b20b631c7dc681e78c38d
Moises Bessalle <mbessalle@gmail.com>
2020-07-16 21:19:58 +0200

    Added trades page routing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import React from "react";
-import "./App.css";
-import Courses from "./pages/Courses";
-import { Switch, Route } from "react-router-dom";
-import SignUp from "./pages/SignUp";
-import Login from "./pages/Login";
-import UserDock from "./components/UserDock";
-import Navbar from "react-bootstrap/Navbar";
-import Nav from "react-bootstrap/Nav";
 import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { Route, Switch } from "react-router-dom";
+import "./App.css";
+import UserDock from "./components/UserDock";
 import CourseDetails from "./pages/CourseDetails";
+import Courses from "./pages/Courses";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+import Trades from "./pages/Trades";
+import { useSelector } from "react-redux";
+import { selectToken } from "./store/user/selector";
 
 function App() {
+  const token = useSelector(selectToken);
   return (
     <div className="App" style={{ backgroundColor: "#aaa" }}>
       <Navbar bg="dark" variant="dark">
-        <Navbar.Brand href="http://localhost:3000">Trading App</Navbar.Brand>
+        <Navbar.Brand href="/">Trading App</Navbar.Brand>
         <Nav className="mr-auto">
-          <Nav.Link href="http://localhost:3000">Home</Nav.Link>
-          <Nav.Link href="http://localhost:3000/trades">Trades</Nav.Link>
+          <Nav.Link href="/">Home</Nav.Link>
+          {token ? <Nav.Link href="/trades">Trades</Nav.Link> : null}
         </Nav>
         <UserDock />
       </Navbar>
@@ -25,6 +29,7 @@ function App() {
         <Route path="/signup" component={SignUp} />
         <Route path="/login" component={Login} />
         <Route path="/course/:id" component={CourseDetails} />
+        <Route path="/trades" component={Trades} />
         <Route path="/" component={Courses} />
       </Switch>
     </div>
---
+++ src/pages/Trades/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { selectToken } from "../../store/user/selector";
+import { useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
+import { useEffect } from "react";
+
+export default function Trades() {
+  const token = useSelector(selectToken);
+  const history = useHistory();
+  console.log("token in /trades", token);
+
+  useEffect(() => {
+    if (token === null) {
+      history.push("/");
+    }
+  }, [token, history]);
+
+  return <h1>Trades</h1>;
+}
